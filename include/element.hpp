288b962d6eaf04b8a817e13a2d03d8f873c92de4
lucjones02 <lucjones@Lucs-MacBook-Air.local>
2025-05-23 20:02:12 +0100

    added constructors and assignment operators to element_of sets and groups

--- include/element.hpp
+++ include/element.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <concepts>
+#include <optional>
+#include <iostream>
 
 #include <primitive_set.hpp>
 #include <group.hpp>
@@ -15,17 +17,80 @@ enum class TYPE
     RING
 };
 
+template <typename S, TYPE type, class Impl> // Impl is for CRTP
+class element_of_base
+{
+public:
+    element_of_base() = default;
+    element_of_base(typename S::value_type _val) : m_value(_val) {};
+    element_of_base(const Impl& _e) : m_value(_e.m_value) {};
+
+    element_of_base& operator=(const Impl& _e) { m_value = _e.m_value; return this; }
+    element_of_base& operator=(typename S::value_type _val) { m_value = _val; return *this; }
+
+    friend std::ostream& operator<<(std::ostream& os, Impl& e)
+    {
+        if (e.has_value())
+        {
+            os << e.m_value.value();
+        }
+        else
+        {
+            os << "no value";
+        }
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& os, Impl& e)
+    {
+        typename S::value_type _val;
+        os >> _val; // TODO: deal with bad inputs
+        e.m_value = _val;
+        return os;
+    }
+
+    bool has_value() const { return m_value.has_value(); }
+
+protected:
+    std::optional<typename S::value_type> m_value;
+
+};
+
 template <typename S, TYPE type>
-class element_of {};
+class element_of : public element_of_base<S, type, element_of<S, type>>
+{
+};
 
 template <typename S> requires std::derived_from<S, primitive_set_t>
-class element_of<S, TYPE::SET>
+class element_of<S, TYPE::SET> : public element_of_base<S, TYPE::SET, element_of<S, TYPE::SET>>
 {
+    using element_t = element_of<S, TYPE::SET>;
+    using base_element_t = element_of_base<S, TYPE::SET, element_t>;
+
+public:
+    element_of() : base_element_t() {}
+    element_of(typename S::value_type _val) : base_element_t(_val) {}
+    element_of(const element_of& _e) : base_element_t(_e) {}
+
+    element_t& operator=(typename S::value_type _val) { base_element_t::operator=(_val); return *this; }
+    element_t& operator=(const element_t& _e) { base_element_t::operator=(_e); return *this; }
+
 };
 
 template <typename G> requires std::derived_from<G, group_t>
 class element_of<G, TYPE::GROUP>
 {
+    using element_t = element_of<G, TYPE::GROUP>;
+    using base_element_t = element_of_base<G, TYPE::GROUP, element_t>;
+
+public:
+    element_of() : base_element_t() {}
+    element_of(typename G::value_type _val) : base_element_t(_val) {}
+    element_of(const element_of& _e) : base_element_t(_e) {}
+
+    element_t& operator=(typename G::value_type _val) { base_element_t::operator=(_val); return *this; }
+    element_t& operator=(const element_t& _e) { base_element_t::operator=(_e); return *this; }
+
 };
 
 } // namespace abstract_algebra
--- src/abstract_algebra.cpp
+++ src/abstract_algebra.cpp
@@ -7,7 +7,14 @@ int main()
 {
     using S = aa::primitive_set<uint32_t, 32>;
     aa::element_of<S, aa::TYPE::SET> e;
+    aa::element_of<S, aa::TYPE::SET> e2 = e;
+    aa::element_of<S, aa::TYPE::SET> e3 = 0;
+    aa::element_of<S, aa::TYPE::SET> e4;
+    e4 = 0;
 
-    using G = aa::group<S>;
+    std::cout << "e  = " << e << std::endl;
+    std::cout << "e3 = " << e3 << std::endl;
+
+    using G = aa::group<S, std::plus<uint32_t>>;
     aa::element_of<G, aa::TYPE::GROUP> g;
 }
