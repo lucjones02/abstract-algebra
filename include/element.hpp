78c2aa1e46385f061729baef83f1b9f052f61cd9
lucjones02 <lucjones@Lucs-MacBook-Air.local>
2025-05-23 22:18:03 +0100

    added addition for groups

--- include/element.hpp
+++ include/element.hpp
@@ -3,23 +3,21 @@
 #include <concepts>
 #include <optional>
 #include <iostream>
+#include <cassert>
 
 #include <primitive_set.hpp>
 #include <group.hpp>
 
-namespace abstract_algebra
-{
+namespace abstract_algebra {
 
-enum class TYPE 
-{
+enum class TYPE {
     SET,
     GROUP,
     RING
 };
 
 template <typename S, TYPE type, class Impl> // Impl is for CRTP
-class element_of_base
-{
+class element_of_base {
 public:
     element_of_base() = default;
     element_of_base(typename S::value_type _val) : m_value(_val) {};
@@ -28,21 +26,17 @@ public:
     element_of_base& operator=(const Impl& _e) { m_value = _e.m_value; return this; }
     element_of_base& operator=(typename S::value_type _val) { m_value = _val; return *this; }
 
-    friend std::ostream& operator<<(std::ostream& os, Impl& e)
-    {
-        if (e.has_value())
-        {
+    friend std::ostream& operator<<(std::ostream& os, Impl& e) {
+        if (e.has_value()) {
             os << e.m_value.value();
         }
-        else
-        {
+        else {
             os << "no value";
         }
         return os;
     }
 
-    friend std::istream& operator>>(std::istream& os, Impl& e)
-    {
+    friend std::istream& operator>>(std::istream& os, Impl& e) {
         typename S::value_type _val;
         os >> _val; // TODO: deal with bad inputs
         e.m_value = _val;
@@ -50,20 +44,19 @@ public:
     }
 
     bool has_value() const { return m_value.has_value(); }
+    S::value_type get_value() const { return m_value.value(); }
+    void set_value(S::value_type _val) { m_value = _val; }
 
-protected:
+private:
     std::optional<typename S::value_type> m_value;
 
 };
 
 template <typename S, TYPE type>
-class element_of : public element_of_base<S, type, element_of<S, type>>
-{
-};
+class element_of : public element_of_base<S, type, element_of<S, type>> {};
 
 template <typename S> requires std::derived_from<S, primitive_set_t>
-class element_of<S, TYPE::SET> : public element_of_base<S, TYPE::SET, element_of<S, TYPE::SET>>
-{
+class element_of<S, TYPE::SET> : public element_of_base<S, TYPE::SET, element_of<S, TYPE::SET>> {
     using element_t = element_of<S, TYPE::SET>;
     using base_element_t = element_of_base<S, TYPE::SET, element_t>;
 
@@ -78,8 +71,7 @@ public:
 };
 
 template <typename G> requires std::derived_from<G, group_t>
-class element_of<G, TYPE::GROUP>
-{
+class element_of<G, TYPE::GROUP> : public element_of_base<G, TYPE::GROUP, element_of<G, TYPE::GROUP>> {
     using element_t = element_of<G, TYPE::GROUP>;
     using base_element_t = element_of_base<G, TYPE::GROUP, element_t>;
 
@@ -91,6 +83,10 @@ public:
     element_t& operator=(typename G::value_type _val) { base_element_t::operator=(_val); return *this; }
     element_t& operator=(const element_t& _e) { base_element_t::operator=(_e); return *this; }
 
+    friend element_t operator+(const element_t& _e1, const element_t& _e2) { 
+        assert(_e1.has_value() && _e2.has_value());
+        return G::addition(_e1.get_value(), _e2.get_value());
+    }
 };
 
 } // namespace abstract_algebra
--- src/abstract_algebra.cpp
+++ src/abstract_algebra.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <functional>
 #include <element.hpp>
 
 namespace aa = abstract_algebra;
@@ -12,9 +13,19 @@ int main()
     aa::element_of<S, aa::TYPE::SET> e4;
     e4 = 0;
 
-    std::cout << "e  = " << e << std::endl;
     std::cout << "e3 = " << e3 << std::endl;
+    std::cout << "e  = " << e << std::endl;
 
-    using G = aa::group<S>;
-    aa::element_of<G, aa::TYPE::GROUP> g;
+    using G = aa::group<S, std::plus<S::value_type>>;
+    aa::element_of<G, aa::TYPE::GROUP> g0;
+    aa::element_of<G, aa::TYPE::GROUP> g1 = 1;
+    aa::element_of<G, aa::TYPE::GROUP> g2 = 2;
+    aa::element_of<G, aa::TYPE::GROUP> g3 = g1 + g2;
+
+    std::cout << "g0 = " << g0 << std::endl;
+    std::cout << "g1 = " << g1 << std::endl;
+    std::cout << "g2 = " << g2 << std::endl;
+    std::cout << "g3 = " << g3 << std::endl;
+
+
 }
