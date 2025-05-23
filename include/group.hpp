9bb8eb9f5bfcf702ca2a17bfcfd2f6e40c2430a4
Luc Jones <luc.xav.jones@gmail.com>
2025-05-23 22:23:35 +0100

    added addition for groups
    added addition for groups
    
    ---------
    
    Co-authored-by: lucjones02 <lucjones@Lucs-MacBook-Air.local>

--- include/group.hpp
+++ include/group.hpp
@@ -10,12 +10,14 @@ namespace abstract_algebra
 
     class group_t {};
 
-    template <class S> requires std::derived_from<S, primitive_set_t>
-    class group : group_t
+    template <class S, typename add> requires std::derived_from<S, primitive_set_t>
+    class group : public group_t
     {
         public:
             typedef typename S::value_type value_type;
             static constexpr uint64_t size = S::size;
+            static constexpr auto addition = add();
+            
     };
 
 }
